ed30d1e240621c0f72e608f8d2535a363614ba4b
dan-cl <djclissold@yahoo.co.uk>
2016-07-27 19:12:40 +0100

    18. Adding user accounts

--- app/app.rb
+++ app/app.rb
@@ -5,8 +5,11 @@ require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
 
+enable :session
+set :session_secret, 'super secret'
+
 get '/' do
-  erb :'links/home'
+  erb :'users/new'
 end
 
 get '/links' do
@@ -32,6 +35,22 @@ get '/tags' do
   tag = Tag.first(tag: params[:tag])
   @links = tag ? tag.links : []
   erb :'links/index'
+end
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  user = User.create(email: params[:email], password: params[:password])
+  session[:user_id] = user.id
+  redirect to('/links')
+end
+
+helpers do
+  def current_user
+    @current_user ||= User.get(session[:user_id])
+  end
 end
 
   # start the server if ruby file executed directly
--- spec/feature/web_helpers.rb
+++ spec/feature/web_helpers.rb
@@ -15,3 +15,11 @@ def add_tag
   fill_in "tag", :with => "video"
   click_button 'submit'
 end
+
+def sign_up
+  visit 'users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :email,     with: 'alice@examples.com'
+  fill_in :password,  with: 'oranges!'
+  click_button 'Sign up'
+end
